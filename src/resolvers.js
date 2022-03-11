6cf1628b3dcab127651b48047f874dff7f049585
German Rodriguez <german.rodriguezdorado@etermax.com.uy>
2022-03-11 11:40:21 -0300

    change schema

--- src/resolvers.js
+++ src/resolvers.js
@@ -36,6 +36,11 @@ const resolvers = {
       }
     },
   },
+  
+  Module: {
+    durationInSeconds: ({ length }) => length,
+  },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
@@ -44,6 +49,9 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    durationInSeconds: ({ length }) => length,
+
   },
 };
 
